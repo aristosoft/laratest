e83b2136321100fb4cadd25803de265062512f88
Ibukun Akinlemibola <ibukunakins@gmail.com>
2015-09-10 13:29:55 +0000

    Composer recent update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5.1 : here we gocle</div>
                 <div class="quote">{{ Inspiring::quote() }}</div>
             </div>
         </div>
